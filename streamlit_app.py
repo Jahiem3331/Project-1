1c6677488d65de98a25e16f8c1ad3c985b352624
Jahiem3331 <Jahiem.Johnson@student.mec.cuny.edu>
2025-11-10 09:43:49 -0600

    I am making a change

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,13 +86,12 @@ elif page == '🤠 About':
   for i, interest in enumerate(interests):
     with cols[i % 3]:
       st.info(f'🔷 {interest}')
-elif page == 'Projects':
+elif page == '💼 Projects':
     st.title ('MY Projects')
     st.Write('Here are some project I have worked on:')
 
-
-
-
+# Project 1 
+with st.container():
 
 
 
